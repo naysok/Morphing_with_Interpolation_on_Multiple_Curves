c06eadad1692685aa7631f99c6bf95583d7356cc
naysok <ysok.ysok@gmail.com>
2019-06-10 03:22:42 +0900

    [update] refactoring

--- Morphing.py
+++ Morphing.py
@@ -1,11 +1,5 @@
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
-import ghpythonlib.components as ghpc
-
-
-seqCrv = []
-render_Pts = []
-RENDER = []
 
 
 
@@ -23,6 +17,7 @@ def divide_crvs(crvs, div_count):
     
     ### 2D Array
     return points_div
+
 
 
 ### Flip 2D Array
@@ -48,156 +43,83 @@ def interp_crvs(array_2d_f):
 
 
 
-### divide interp curve
-def divide_interp_crv(crvs, pts_div_crv_flip):
+### Slice Sequence
+def slice_sequence(crvs, seq_count):
     
-    # print len(crvs)
-    # print len(pts_div_crv_flip)
+    pt_slice = []
     
-    crv_split_array = []
+    frame = 1 / seq_count
+    # print frame
+    
     
     for i in xrange(len(crvs)):
         crv = crvs[i]
-        pts = pts_div_crv_flip[i]
-        # print pts
+        crv = rs.coercecurve(crv)
+        start_pt, end_pt = rs.CurveDomain(crv)
+        # print start_pt, "to", end_pt ### [0 to len(Curves)]
         
-        param_split_pts = []
-        for j in xrange(len(pts)):
-            param_on_crv = rs.CurveClosestPoint(crv, pts[j])
-            param_split_pts.append(param_on_crv)
-        print param_split_pts
-        print "---"
+        param_slice = []
+        for j in xrange(int(end_pt)):
+            
+            for k in xrange(seq_count):
+                value = j + k*frame
+                param_slice.append(value)
         
-        ### slice k - k+1
-        crv_split = rs.SplitCurve(crv, param_split_pts)
+        param_slice.append(end_pt)
         
-        crv_split_array.append(crv_split)
+        # print param_slice
+        # print len(param_slice)
+        
+        sub_list = []
+        for l in xrange(len(param_slice)):
+            pt = rg.Curve.PointAt(crv, param_slice[l])
+            sub_list.append(pt)
+        
+        pt_slice.append(sub_list)
     
-    return crv_split_array
+    return pt_slice
+
+
+
+### Generate Frame
+def generate_frame(pt_array):
+    
+    crv_frame = []
+    for i in xrange(len(pt_array)):
+        
+        pt_list = []
+        tmp_list = pt_array[i]
+        
+        ### closed, add -1
+        for j in xrange(len(tmp_list)):
+            pt_list.append(tmp_list[j])
+        
+        pt_list.append(tmp_list[0])
+        
+        crv = rs.AddInterpCurve(pt_list)
+        rs.CloseCurve(crv)
+        crv_frame.append(crv)
+    
+    return crv_frame
+
+
+
 
 
 
 pts_div_crv = divide_crvs(CURVES, DIV_COUNT)
 pts_div_crv_flip = flip_2d_array(pts_div_crv)
-crvs = interp_crvs(pts_div_crv_flip)
-crvs_segment = divide_interp_crv(crvs, pts_div_crv_flip)
 
-debug = crvs_segment[3]
+crvs_interpolate = interp_crvs(pts_div_crv_flip)
 
+pt_slice = slice_sequence(crvs_interpolate, SEQ_COUNT)
+pt_slice_flip = flip_2d_array(pt_slice)
 
-#hoge = []
-#for num in xrange(len(pts_div_crv_flip)):
-#    print num
-#    hoge.append(rs.AddInterpCurve(pts_div_crv_flip[num]))
-#
-#for num in xrange(len(pts_div_crv)):
-#    print num
-#    hoge.append(rs.AddInterpCurve(pts_div_crv[num]))
-
-#debug = hoge
+crvs_all_frame = generate_frame(pt_slice_flip)
 
 
 
-
-"""
-### Flipping Matrix, ((0,10,20), (1,11,21), ...)
-for i in xrange(divCount):
-    flipMatPts = []
-    flipMat_Line_Z = []
-    
-    for j in xrange(len(crvs)):
-        # print(int(j*divCount + i))
-        tmp_mat = divCrvPts[int(j*divCount + i)]
-        tmp_mat_Add_Z_src = rs.AddPoint((tmp_mat.X, tmp_mat.Y, tmp_mat.Z))
-        tmp_mat_Add_Z = rs.coerce3dpoint(tmp_mat_Add_Z_src)
-        tmp_mat_Add_Z.Z = 1.0
-        
-        tmp_Line = rs.AddLine(tmp_mat, tmp_mat_Add_Z)
-        flipMatPts.append(tmp_mat)
-        flipMat_Line_Z.append(tmp_Line)
-    
-    #print(len(flipMatPts))
-    #print(len(flipMat_Line_Z))
-    #print("- - -")
-    
-    tmp_interpCrv = rs.AddInterpCurve(flipMatPts)
-    tmp_interpCrv_coerce = rs.coercecurve(tmp_interpCrv)
-    tmp_interpCrv_coerce.Domain = rg.Interval(0,1)
-    
-    
-    
-    
-    
-    
-    all_v2 = []
-    Result_v2 = []
-    
-    for k in xrange(len(flipMat_Line_Z)):
-        flipMat_Line_Z_coerce = rs.coercecurve(flipMat_Line_Z[k])
-        v1, v2 ,v3 = ghpc.CurveXCurve(tmp_interpCrv_coerce, flipMat_Line_Z_coerce)
-        #print("v2", v2)
-        all_v2.append(v2)
-    
-    #debug = all_v2
-    
-    tmp_Result = 0
-    
-    for l in xrange(len(all_v2)):
-        tmp_List = all_v2[l]
-        if type(tmp_List) is list:
-#            print(tmp_List)
-#            print(tmp_Result)
-            Result_v2.append(tmp_List[tmp_Result])
-            tmp_Result+=1
-        else:
-            Result_v2.append(tmp_List)
-    
-    #print(Result_v2)
-    #print(len(Result_v2))
-#    print(type(Result_v2))
-#    print(tmp_interpCrv_coerce)
-
-    
-    shatterCrv = ghpc.Shatter(tmp_interpCrv_coerce, Result_v2)
-    #print(len(shtterCrv))
-    #print(type(shtterCrv))
-    
-    
-    Final_Pts = []
-    for m in xrange(len(shatterCrv)):
-        tmp_Final_Pts = rs.DivideCurve(shatterCrv[m], SeqCount)
-        #pritn(len(tmp_Final_Pts))
-        
-        for n in xrange(len(tmp_Final_Pts)):
-            Final_Pts.append(tmp_Final_Pts[n])
-        
-    #print(Final_Pts)
-    #print(len(Final_Pts))
-    
-    for o in xrange(len(Final_Pts)):
-        render_Pts.append(Final_Pts[o])
-
-
-
-#print(len(render_Pts))
-#print(len(Final_Pts))
-#print(len(crvs))
-
-### Render
-for i in xrange(len(Final_Pts)):
-    Render_Curve_Pts = []
-    for j in xrange(divCount):
-        #print(int(j*len(Final_Pts) + i))
-        tmp_render_pt = render_Pts[int(j*len(Final_Pts) + i)]
-        Render_Curve_Pts.append(tmp_render_pt)
-    #print(len(Render_Curve_Pts))
-    #RENDER_CRV = rs.AddInterpCurve(Render_Curve_Pts)
-    RENDER_CRV, tmp_999, tmp_998 = ghpc.Interpolate(Render_Curve_Pts, 3, True)
-    RENDER.append(RENDER_CRV)
-
-
-crvs_w_sequence = RENDER
-calcDomain = rg.Interval(0,len(RENDER)-1)
-lastFrame = len(RENDER)-1
-
-"""
+### Output
+CRUVES_w_SEQ = crvs_all_frame
+DOMAIN = rg.Interval(0, len(crvs_all_frame)- 1)
+LAST_FRAME = len(crvs_all_frame)
